e858038334de21efa4b3be3b85345a583b23eef6
Marek Laslo <marek.laslo@icloud.com>
2025-04-12 00:35:34 +0200

    print resistor values

--- SensorSynth/SensorSynth.cpp
+++ SensorSynth/SensorSynth.cpp
@@ -1,19 +1,30 @@
 #include "daisy_seed.h"
+#include "daisysp.h"
 
 using namespace daisy;
+using namespace daisysp;
 
 DaisySeed hw;
 
 int main(void) {
 
-  // Initialize the Daisy Seed Hardware
   hw.Init();
 
-  // Enable Logging, and set up the USB connection.
+  AdcChannelConfig adc_cfg;
+  adc_cfg.InitSingle(daisy::seed::A0);
+  hw.adc.Init(&adc_cfg, 1);
+  hw.adc.Start();
+
   hw.StartLog(true);
 
-  // And Print Hello World!
   hw.PrintLine("Hello World! This is the Daisy Seed!");
 
-  while(1) {}
+  while(1) {
+
+    float analog_value = hw.adc.GetFloat(0);
+
+    // Print the value to the log
+    //!!! MAKEFILE: DISABBLE LOGGING FOR BETTER PERFORMANCE
+    hw.PrintLine("Analog Value (A0): %f", analog_value);
+  }
 }
