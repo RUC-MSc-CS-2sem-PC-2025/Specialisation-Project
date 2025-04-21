857660ed5f6c6174281a0540bbe1fe9039f844fc
Marek Laslo <marek.laslo@icloud.com>
2025-04-21 13:05:02 +0200

    working on sound design

--- SensorSynth/SynthLib/harmonydrone.cpp
+++ SensorSynth/SynthLib/harmonydrone.cpp
@@ -2,22 +2,59 @@
 
 using namespace sensorsynth;
 
-void HarmonyDrone::InitOsc(float sample_rate) 
+HarmonyDrone::HarmonyDrone(float samplerate, float key_freq)
 {
-    osc1.Init(sample_rate);
-    osc1.SetWaveform(osc1.WAVE_SIN);
-    osc1.SetFreq(110);
-    osc1.SetAmp(1);
+    sample_rate_ = samplerate;
+    key_freq_ = key_freq;
+};
+
+void HarmonyDrone::SetKeyFreq(float freq)
+{
+    key_freq_ = freq;
+};
+
+float HarmonyDrone::GetKeyFreq()
+{
+    return key_freq_;
+};
+
+void HarmonyDrone::SetSampleRate(float sample_rate)
+{
+    sample_rate_ = sample_rate;
+};
+
+float HarmonyDrone::GetSampleRate()
+{
+    return sample_rate_;
+};
+
+void HarmonyDrone::InitOsc(daisysp::Oscillator &osc, float sample_rate, float freq = 440, float amp = 1, uint8_t waveform = 0)
+{
+    osc.Init(sample_rate);
+    osc.SetWaveform(waveform);
+    osc.SetFreq(freq);
+    osc.SetAmp(amp);
+};
+
+void HarmonyDrone::Init()
+{
+    float sample_rate = GetSampleRate();
+
+    float key_freq = GetKeyFreq();
+    float half_key_freq = key_freq * 0.5f;
+    float double_key_freq = key_freq * 2.0f;
+    float quadruple_key_freq = key_freq * 4.0f;
+
+    InitOsc(osc1, sample_rate, key_freq * 0.5f, 1, daisysp::Oscillator::WAVE_SIN);
+    InitOsc(osc2, sample_rate, half_key_freq, 1, daisysp::Oscillator::WAVE_SIN);
+    InitOsc(osc3, sample_rate, double_key_freq * 2, 1, daisysp::Oscillator::WAVE_SIN);
+    InitOsc(osc4, sample_rate, quadruple_key_freq * 4, 1, daisysp::Oscillator::WAVE_SIN);
+
+    InitOsc(lfo1, sample_rate, 0.5f, 1, daisysp::Oscillator::WAVE_SIN);
+    InitOsc(lfo2, sample_rate, 0.25f, 1, daisysp::Oscillator::WAVE_SIN);
 };
 
 float HarmonyDrone::Process(float sample_rate)
 {
-
-
-    lfo1.Init(sample_rate);
-    lfo1.SetWaveform(lfo1.WAVE_SIN);
-    lfo1.SetFreq(0.5f);
-    lfo1.SetAmp(1);
-
-    osc1.SetFreq(440 * (photo1.GetValue() * 2.0f));
+    return 0.0f;
 };
